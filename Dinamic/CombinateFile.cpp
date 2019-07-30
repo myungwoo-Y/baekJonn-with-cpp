dd9c7d8da62a45576ffc81b6c8ec1c9072029909
myungwoo-Y <auddn6676@naver.com>
2019-07-30 16:43:07 +0900

    refactoring combinate file

--- Dinamic/CombinateFile.cpp
+++ Dinamic/CombinateFile.cpp
@@ -1,43 +1,37 @@
-#include <iostream>
-#include "ostream"
-#include "algorithm"
-
+#include<cstdio>
+#include<algorithm>
+#include <cstring>
+#include <cmath>
+#include <climits>
 using namespace std;
-
-int combinateFile() {
-    ios_base::sync_with_stdio(false);
-    int n, k, y, t;
-    //auto dp = new int[500][500];
-    int dp[500][500];
-    cin >> n;
-    while (n--) {
-        cin >> k;
-        int sum[k];
-        int temp;
-        cin >> sum[0];
-        for(int i = 1; i < k; i++){
-            cin >> sum[i];
+int dp[501][501];
+int sum[501];
+int main() {
+    int n, fileNumber, end;
+    scanf("%d", &n);
+    for (; n > 0; --n) {
+        scanf("%d", &fileNumber);
+        for(int i = 1; i <= fileNumber; i++){
+            scanf("%d", &sum[i]);
             sum[i] += sum[i-1];
         }
-
-        for(int d = 1; d < k; d++){
-            for(int x = 0; x+d < k; x++){
-                y = x+d;
-                dp[x][y] = INT32_MAX;
-                for(int t = x; t < y; t++){
-                    int a = dp[x][t];
-                    int b = dp[t+1][y];
-                    int c =(sum[y] - sum[x]);
-                    if(x == 0){
-                        temp = 0;
-                    }else{
-                        temp = sum[x-1];
-                    }
-                    dp[x][y] = min(dp[x][y], dp[x][t] + dp[t+1][y] + (sum[y] - temp));
+        // 기존 값과 차이를 1 부터 최대까지 하여 조사
+        for (int weight = 1; weight < fileNumber; ++weight) {
+            // 1번째 책부터 최대 번호 책까지 차이에 대한 최솟값 구한다.
+            for (int base = 1; base + weight <= fileNumber; ++base) {
+                end = base+weight;
+                dp[base][end] = INT_MAX;
+                // 범위 내에서 구간을 2분할 수 있는 모든 경우의 값을 구하여 나온 최솟값 저장
+                for (int pivot = base; pivot < end; ++pivot) {
+                    dp[base][end] = min(dp[base][end],
+                                        dp[base][pivot] + dp[pivot+1][end] + (sum[end] - sum[base-1]));
                 }
             }
         }
-        cout << dp[0][k-1] << endl;
+        printf("%d\n", dp[1][fileNumber]);
     }
     return 0;
 }
+
+
+
